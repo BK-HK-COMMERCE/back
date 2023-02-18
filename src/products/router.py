c204502956738f4c78b7b0cc0dca3ac1962524d0
hakyoung0926 <43024383+hakyoung0926@users.noreply.github.com>
2023-02-18 22:13:56 +0900

    삭제 기능  수정, 추가

--- src/products/router.py
+++ src/products/router.py
@@ -1,6 +1,6 @@
 import uuid
 
-from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi import APIRouter, Depends, HTTPException, Body, Query
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -16,8 +16,6 @@ router = APIRouter(
 @router.get("/", response_model=list[ProductBase])
 def get_all_products(db: Session = Depends(get_db)):
     products = db.query(Product).all()
-    if products is None:
-        raise HTTPException(404, "없음")
     return products
 
 
@@ -34,3 +32,15 @@ def create_product(db: Session = Depends(get_db), product_base: ProductBaseInput
     db.refresh(db_product)
 
     return {"response": db_product}
+
+
+@router.delete("/{index_no}")
+def delete_product(db: Session = Depends(get_db), index_no: int = Query()):
+    db_product = db.query(Product).filter(Product.index_no == index_no).first()
+    if db_product is None:
+        raise HTTPException(404, "Product is not found")
+
+    db.delete(db_product)
+    db.commit()
+    return {"response": f"{index_no} Product is deleted"}
+
