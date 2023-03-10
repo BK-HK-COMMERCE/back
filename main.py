8e524147c8f88ab120efd409b0ba3612023e9bf0
hakyoung0926 <43024383+hakyoung0926@users.noreply.github.com>
2023-03-10 13:55:01 +0900

    cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.products.router import router as products_router
 from src.users.router import router as users_router
 from src.auth.router import router as auth_router
@@ -11,3 +12,14 @@ app.include_router(products_router)
 app.include_router(users_router)
 app.include_router(auth_router)
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
